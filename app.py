02138dc6fb9b7a00fe8f253c718d0811b55ca526
Shateizin <86153752+Shateizin@users.noreply.github.com>
2022-03-24 13:41:26 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup as bs
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+CORS(app)
+@app.route('/nft/<string:endereco>/')
+def consul_nft(endereco):
+	url = f"https://api.nftport.xyz/v0/accounts/{endereco}?chain=polygon"
+	header = {"Content-Type": "application/json", "Authorization": "9e1339fe-e3e2-48b5-811f-2efb37253304"}
+	ak = requests.get(url, headers=header)
+	res = ak.json()
+	res = res['nfts']
+	nft = {'nft': []}
+	for c in res:
+		nfts = c['name']
+		if nfts == "":
+			pass
+		else:
+			nft['nft'].append(nfts)
+	return jsonify(nft)
+	
+if __name__ == "__main__":
+	app.run()
